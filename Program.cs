a3b1b083b0835792d4486e16ba05413cf5f715d7
jose felipe da silva santos <josefelipesilvasantos874@gmail.com>
2022-03-04 22:31:34 -0300

    Finaliza exercício

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(@"Yeah, I'd rather be a lover than a fighter (fighter)
+'Cause all my life, I've been fighting
+Never felt a feeling of comfort, oh
+And all this time, I've been hiding
+And I never had someone to call my own, oh nah
+I'm so used to sharing
+Love only left me alone
+But I'm at one with the silence");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(@"I found peace in your violence
+Can't tell me there's no point in trying
+I'm at one, and I've been quiet for too long
+I found peace in your violence
+Can't tell me there's no point in trying
+I'm at one, and I've been silent for too long");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine(@"I've been quiet for too long
+I've been quiet for too long
+I found peace in your violence
+Can't tell me there's no point in trying
+I'm at one, and I've been quiet for too long");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine(@"I'm in need of a savior (savior), but I'm not asking for favors
+My whole life, I've felt like a burden
+I think too much, and I hate it
+I'm so used to being in the wrong, I'm tired of caring
+Loving never gave me a home, so I'll sit here in the silence");
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(@"I found peace in your violence
+Can't tell me there's no point in trying
+I'm at one, and I've been quiet for too long
+I found peace in your violence
+Can't tell me there's no point in trying
+I'm at one, and I've been silent for too long");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(@"I've been quiet for too long
+I've been quiet for too long
+I found peace in your violence
+Can't tell me there's no point in trying
+I'm at one, and I've been quiet for too long");
+Console.ResetColor();
